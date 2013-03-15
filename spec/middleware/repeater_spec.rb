87f699fc5375b2e3b28b6ba2d17cfd818872b0f1
iain <iain@iain.nl>
2013-03-15 12:00:32 +0100

    Handle other errors too

--- spec/middleware/repeater_spec.rb
+++ spec/middleware/repeater_spec.rb
@@ -26,6 +26,20 @@ describe Faraday::Conductivity::Repeater do
     get_with_max(3)
   end
 
+  it "handles other errors too" do
+    connection = Faraday.new(url: "http://blabla.bla") { |faraday|
+      faraday.use :repeater, mode: :rapid, retries: 2
+      faraday.adapter Faraday.default_adapter
+    }
+
+    pattern = double :pattern
+    Faraday::Conductivity::Repeater::Pattern.should_receive(:new).and_return(pattern)
+    pattern.should_receive(:wait).with(1).ordered
+    pattern.should_receive(:wait).with(2).ordered
+
+    expect { connection.get("/") }.to raise_error(Faraday::Error::ConnectionFailed)
+  end
+
   def get_with_max(num)
     connection.get("/unreliable/#{num}") { |req|
       req.options[:timeout] = 1
