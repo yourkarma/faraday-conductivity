accb1e89a95e62b3794040c726afc60d215c69e9
iain <iain@iain.nl>
2014-11-17 13:03:45 +0100

    Alias filter to before, to support Rails 4.
    It's an alias so it won't break support for Rails 3.
    
    Closes #4

--- lib/faraday/conductivity/request_id_filter.rb
+++ lib/faraday/conductivity/request_id_filter.rb
@@ -2,8 +2,11 @@ module Faraday
   module Conductivity
     class RequestIdFilter
 
-      def self.filter(controller)
-        Thread.current[:request_id] = controller.request.uuid
+      class << self
+        def filter(controller)
+          Thread.current[:request_id] = controller.request.uuid
+        end
+        alias_method :before, :filter
       end
 
     end
