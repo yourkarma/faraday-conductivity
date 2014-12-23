aa98b4386b3ba6b3c7aa49ed5abd96800b9e19d6
Alexei Mikhailov <a.mikhailov@fun-box.ru>
2014-12-23 12:49:28 +0500

    Add ability to set X-Request-Id via argument, not just thread variable

--- lib/faraday/conductivity/request_id.rb
+++ lib/faraday/conductivity/request_id.rb
@@ -2,12 +2,13 @@ module Faraday
   module Conductivity
     class RequestId < Faraday::Middleware
 
-      def initialize(app)
+      def initialize(app, request_id = nil)
         super(app)
+        @request_id = request_id
       end
 
       def call(env)
-        request_id = Thread.current[:request_id]
+        request_id = @request_id || Thread.current[:request_id]
         if request_id
           env[:request_headers]['X-Request-Id'] ||= request_id
         end
